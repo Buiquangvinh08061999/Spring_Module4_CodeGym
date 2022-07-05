6b00174b4f9b23168d0291dad8d4a19b3f900c45
Bui Quang Vinh <top.xom.ray@gmail.com>
2022-07-05 10:11:51 +0700

    Update Customer

---
+++ 02_Spring Controller/practice/customer-management/src/test/java/cg/wbd/grandemonstration/model/CustomerTest.java
@@ -0,0 +1,12 @@
+package cg.wbd.grandemonstration.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    @Test
+    void getId() {
+    }
+}
